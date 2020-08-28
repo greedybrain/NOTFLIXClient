37c247c892bdd660fec86fe99b75fadec99004f8
Naya Willis <whatyoucodingboutwillis@Nayas-MacBook-Pro.local>
2020-08-28 09:13:00 -0400

    created state with prop of movies

--- src/Pages/Home/AllMovies/containers/AllMoviesContainer.jsx
+++ src/Pages/Home/AllMovies/containers/AllMoviesContainer.jsx
@@ -1,17 +1,33 @@
 import React, { PureComponent } from 'react'
+import axios from 'axios'
+import { getFrom } from '../../../../Tools/config'
+import MovieList from '../../../../Global/components/MovieList'
 
 class AllMoviesContainer extends PureComponent {
     constructor(props) {
         super(props)
 
         this.state = {
-            
+            movies: []
         }
     }
 
+    componentDidMount() {
+        let { baseUrl, parameters} = getFrom.endpoints
+        axios(`${baseUrl}${parameters.allMovies}`)
+            .then(response => {
+                this.setState({
+                    movies: response.data.movies.data
+                })
+            })
+    }
+
     render() {
+        let { movies } = this.state
         return (
-            
+            <div className="home-movie-list">
+                <MovieList movies={movies} />
+            </div>
         )
     }
 }
