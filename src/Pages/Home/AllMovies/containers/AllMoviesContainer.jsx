a9183d554ea4e6836faec333de6771282c51cc20
Naya Willis <whatyoucodingboutwillis@Nayas-MacBook-Pro.local>
2020-08-28 08:24:24 -0400

    created AllMoviesContainer

---
+++ src/Pages/Home/AllMovies/containers/AllMoviesContainer.jsx
@@ -0,0 +1,19 @@
+import React, { PureComponent } from 'react'
+
+class AllMoviesContainer extends PureComponent {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            
+        }
+    }
+
+    render() {
+        return (
+            
+        )
+    }
+}
+
+export default AllMoviesContainer
