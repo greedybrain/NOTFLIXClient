970782cedc12217ff7a99ac258d229c9a7090f7e
Naya Willis <whatyoucodingboutwillis@Nayas-MacBook-Pro.local>
2020-08-28 22:56:41 -0400

    rearranged order of image div and other data

--- src/Global/components/Movie.jsx
+++ src/Global/components/Movie.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 
 const Movie = ({movie}) => {
     return (
-        <li key={movie.id}>
-            <h1>{movie.attributes.title}</h1>
-            <h3>{movie.attributes.release_year}</h3>
-            <h3>{movie.attributes.plot}</h3>
-            <img src={movie.attributes.poster} alt="Hulk"/>
+        <li key={movie.id} className="movie-info">
+            <div className="cover">
+                <img src={movie.attributes.poster} alt="Hulk"/>
+            </div>
+            <div className="meta-data">
+                <h1>{movie.attributes.title}</h1>
+                <h3>{movie.attributes.release_year}</h3>
+                <h3>{movie.attributes.plot}</h3>
+            </div>
         </li>
     )
 }
