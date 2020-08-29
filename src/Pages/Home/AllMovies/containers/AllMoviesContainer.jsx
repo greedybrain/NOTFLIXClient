a3b2ceaf2a4a22cfa7263278977eb7875019b503
Naya Willis <whatyoucodingboutwillis@Nayas-MacBook-Pro.local>
2020-08-28 22:57:11 -0400

    refactored contructor

--- src/Pages/Home/AllMovies/containers/AllMoviesContainer.jsx
+++ src/Pages/Home/AllMovies/containers/AllMoviesContainer.jsx
@@ -1,20 +1,24 @@
-import React, { PureComponent } from 'react'
+import React, { Component } from 'react'
 import axios from 'axios'
-import { getFrom } from '../../../../Tools/config'
 import MovieList from '../../../../Global/components/MovieList'
+import SearchForm from '../components/SearchForm'
 
-class AllMoviesContainer extends PureComponent {
+class AllMoviesContainer extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
             movies: []
         }
+
+        this.apiKey = '691b7eac'
+        this.baseUrl = 'http://localhost:3001/'
+        this.thirdPartyBaseUrl = 'http://www.omdbapi.com/?'
+        this.allMovies = 'api/v1/movies'    
     }
 
     componentDidMount() {
-        let { baseUrl, parameters} = getFrom.endpoints
-        axios(`${baseUrl}${parameters.allMovies}`)
+        axios(`${this.baseUrl}${this.allMovies}`)
             .then(response => {
                 this.setState({
                     movies: response.data.movies.data
@@ -22,11 +26,24 @@ class AllMoviesContainer extends PureComponent {
             })
     }
 
+    movieByTitleAndYear = (title, year)  => {
+        return `${this.thirdPartyBaseUrl}t=${title}&y=${year}&plot=full&apikey=${this.apiKey}`
+}
+    
+    movieByTitle = title => {
+        return `${this.thirdPartyBaseUrl}t=${title}&plot=full&apikey=${this.apiKey}`
+    }
+
     render() {
         let { movies } = this.state
         return (
-            <div className="home-movie-list">
-                <MovieList movies={movies} />
+            <div className="homepage">
+                <div className="search-form">
+                    <SearchForm movieByTitle={this.movieByTitle} movieByTitleAndYear={this.movieByTitleAndYear} />
+                </div>
+                <div className="home-movie-list">
+                    <MovieList movies={movies} />
+                </div>
             </div>
         )
     }
