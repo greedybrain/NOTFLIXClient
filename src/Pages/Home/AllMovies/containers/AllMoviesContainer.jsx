934e2e6b7ef2af7969371d6ecf7f970547420eff
Naya Willis <whatyoucodingboutwillis@Nayas-MacBook-Pro.local>
2020-08-30 13:17:10 -0400

    added movieInfo property to state

--- src/Pages/Home/AllMovies/containers/AllMoviesContainer.jsx
+++ src/Pages/Home/AllMovies/containers/AllMoviesContainer.jsx
@@ -7,18 +7,36 @@ class AllMoviesContainer extends Component {
     constructor(props) {
         super(props)
 
+        // INITIAL STATE 
         this.state = {
-            movies: []
+            movies: [],
+            title: '',
+            year: '',
+            movieInfo: {
+                title: '',
+                actors: '',
+                genre: '',
+                language: '',
+                country: '',
+                runtime: '',
+                releaseDate: '',
+                plot: '',
+                poster: '',
+                imdbRating: '', 
+                production: ''
+            }
         }
 
+        // ENDPOINTS 
         this.apiKey = '691b7eac'
         this.baseUrl = 'http://localhost:3001/'
         this.thirdPartyBaseUrl = 'http://www.omdbapi.com/?'
-        this.allMovies = 'api/v1/movies'    
+        this.allMyBackendMovies = 'api/v1/movies'   
+        this.results = 'api/v1/results'
     }
 
     componentDidMount() {
-        axios(`${this.baseUrl}${this.allMovies}`)
+        axios(`${this.baseUrl}${this.allMyBackendMovies}`)
             .then(response => {
                 this.setState({
                     movies: response.data.movies.data
@@ -34,12 +52,46 @@ class AllMoviesContainer extends Component {
         return `${this.thirdPartyBaseUrl}t=${title}&plot=full&apikey=${this.apiKey}`
     }
 
+    validateEndpointThenRequestMovieInfo = (title, year) => {
+        if (title === '') {
+            alert("Please enter a title")
+            return
+        }
+        if (title !== '' && year === '') {
+            axios(`${this.movieByTitle(title)}`)
+                .then(response => {
+                    this.addMovieInfoToState(response)
+                })
+        }
+        if (title !== '' && year !== '') {
+            axios(`${this.movieByTitleAndYear(title, year)}`)
+                .then(response => {
+                    this.addMovieInfoToState(response)
+                })
+        }
+    }
+
+    addMovieInfoToState = (response) => {
+        this.setState({
+            movieInfo: {
+                title: response.data.Title,
+                actors: response.data.Actors,
+                genre: response.data.Genre,
+                language: response.data.Language,
+                country: response.data.Country,
+                runtime: response.data.Runtime,
+                imdbRating: response.data.imdbRating,
+                production: response.data.Production
+            }
+        })
+    }
+
     render() {
         let { movies } = this.state
         return (
             <div className="homepage">
                 <div className="search-form">
-                    <SearchForm movieByTitle={this.movieByTitle} movieByTitleAndYear={this.movieByTitleAndYear} />
+                    <SearchForm validateEndpointThenRequestMovieInfo={this.validateEndpointThenRequestMovieInfo}/>
                 </div>
                 <div className="home-movie-list">
                     <MovieList movies={movies} />
