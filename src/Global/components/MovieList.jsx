ea98a3820d8a07736677f1c26415e8114e4b346c
Naya Willis <whatyoucodingboutwillis@Nayas-MacBook-Pro.local>
2020-08-31 00:25:43 -0400

    removed sort functionality

--- src/Global/components/MovieList.jsx
+++ src/Global/components/MovieList.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Movie from './Movie'
 
-const MovieList = ({ movies }) => {
-    const allMovies = movies.sort((a, b) => {
-        return a.attributes.title.localeCompare(b.attributes.title)
-    })
-    const renderMovies = allMovies.map(movie => {
+const MovieList = ({ movies, }) => {
+    // const allMovies = movies.sort((a, b) => {
+    //     return a.attributes.title.localeCompare(b.attributes.title)
+    // })
+    const renderMovies = movies.map(movie => {
+        // debugger
         return <Movie key={movie.id} movie={movie} />
     })
     return (
