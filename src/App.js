f20cddf581e0ae363179ff5b032c90c455a96693
Naya Willis <whatyoucodingboutwillis@Nayas-MacBook-Pro.local>
2020-08-31 14:26:51 -0400

    added routing to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import AllMoviesContainer from './Pages/Home/AllMovies/containers/AllMoviesContainer';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import EntryPage from './Pages/LoginSIgnupPage/containers/EntryPage';
 
 function App() {
   return (
     <div className="App">
-      <AllMoviesContainer />
+      <Router>
+        <Switch>
+
+          {/* Route to all movies page  */}
+          <Route
+            exact
+            path="/all_movies"
+            render={
+              props => <AllMoviesContainer {...props} />
+            }
+          />
+        
+          {/* Route to login and signup page  */}
+          <Route
+            exact
+            path="/"
+            render={
+              props => <EntryPage {...props}  />
+            }
+          />
+        </Switch>
+      </Router>
     </div>
   );
 }
