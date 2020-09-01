1359a594af40875cd53830fdce020c1efebe1365
Naya Willis <whatyoucodingboutwillis@Nayas-MacBook-Pro.local>
2020-08-31 20:11:36 -0400

    created login component

--- src/Pages/LoginSIgnupPage/components/Login.jsx
+++ src/Pages/LoginSIgnupPage/components/Login.jsx
@@ -1,10 +1,67 @@
-import React from 'react'
+import React, { useState, } from 'react'
+import Signup from './Signup'
 
-const Login = () => {
+const Login = ({ toggleForm }) => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleEmailChange = event => {
+        setEmail(event.target.value)
+    }
+
+    const handlePasswordChange = event => {
+        setPassword(event.target.value)
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault()
+    }
+
     return (
-        <div>
-            
-        </div>
+        <form onSubmit={handleSubmit}>
+            <div className="fields-wrapper">
+                <h1>Sign In</h1>
+                <div className="email">
+                    <input
+                        onChange={handleEmailChange}
+                        type="email"
+                        placeholder="Email"
+                        value={email}
+                    />
+                </div>
+                <div className="password">
+                    <input
+                        onChange={handlePasswordChange}
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                    />
+                </div>
+                <div className="submit-btn">
+                    <button type="submit">Sign In</button>
+                </div>
+                <div className="remember-help">
+                    <div className="remember">
+                        <input type="checkbox" id="remember" name="remember"/>
+                        <label htmlFor="remember">Remember me</label>
+                    </div>
+                    <div className="help">
+                        Need help?
+                    </div>
+                </div>
+                <div className="signup-instead">
+                    <div className="new-to-netflix">
+                        New to Netflix? 
+                    </div>
+                    <div className="signup" onClick={toggleForm}>
+                        Sign up now.
+                    </div>
+                </div>
+                <div className="captcha">
+                    This page is protected by Google reCAPTCHA to ensure you're not a bot. <span className="learn-more">Learn more</span>.
+                </div>
+            </div>
+        </form>
     )
 }
 
