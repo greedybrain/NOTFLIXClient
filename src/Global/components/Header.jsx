945a911d1b8876dcd026040312212ef2e9e23ff1
Naya Willis <whatyoucodingboutwillis@Nayas-MacBook-Pro.local>
2020-09-01 08:06:01 -0400

    added navlinks

--- src/Global/components/Header.jsx
+++ src/Global/components/Header.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import { Logo } from './Logo'
 import '../../assets/Header/header.css'
+import { Link, NavLink } from 'react-router-dom'
 
 const Header = () => {
     return (
         <header>
             <div className="logo-area">
-                <Logo />
+                <div className="logo">
+                    <Logo />
+                </div>
+                <div className="home">
+                    <NavLink to='/all_movies' activeClassName="active-nav">Movies</NavLink>
+                </div>
+            </div>
+            <div className="nav">
+                <ul>
+                    <li><Link to='/' >Login</Link></li>
+                    <li><Link to='/' >Sign Up</Link></li>
+                </ul>
             </div>
         </header>
     )
