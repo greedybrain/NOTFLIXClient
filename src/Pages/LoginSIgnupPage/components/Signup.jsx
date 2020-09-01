026fcea5abc4dc324285796773fcc70357afd3d7
Naya Willis <whatyoucodingboutwillis@Nayas-MacBook-Pro.local>
2020-08-31 20:11:59 -0400

    added form footer

--- src/Pages/LoginSIgnupPage/components/Signup.jsx
+++ src/Pages/LoginSIgnupPage/components/Signup.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-const Signup = () => {
+const Signup = ({ toggleForm }) => {
     const [username, setUsername] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
@@ -23,29 +23,46 @@ const Signup = () => {
     
     return (
         <form onSubmit={handleSubmit}>
-            <div className="username">
-                <input
-                    onChange={handleUsernameChange}
-                    type="text"
-                    placeholder="Username"
-                    value={username}
-                />
-            </div>
-            <div className="email">
-                <input
-                    onChange={handleEmailChange}
-                    type="email"
-                    placeholder="Email"
-                    value={email}
-                />
-            </div>
-            <div className="password">
-                <input
-                    onChange={handlePasswordChange}
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                />
+            <div className="fields-wrapper">
+            <h1>Sign Up</h1>
+                <div className="username">
+                    <input
+                        onChange={handleUsernameChange}
+                        type="text"
+                        placeholder="Username"
+                        value={username}
+                    />
+                </div>
+                <div className="email">
+                    <input
+                        onChange={handleEmailChange}
+                        type="email"
+                        placeholder="Email"
+                        value={email}
+                    />
+                </div>
+                <div className="password">
+                    <input
+                        onChange={handlePasswordChange}
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                    />
+                </div>
+                <div className="submit-btn">
+                    <button type="submit">Sign Up</button>
+                </div>
+                <div className="signup-instead">
+                    <div className="new-to-netflix">
+                        Already have an account? 
+                    </div>
+                    <div className="signup" onClick={toggleForm}>
+                        Login.
+                    </div>
+                </div>
+                <div className="captcha">
+                    This page is protected by Google reCAPTCHA to ensure you're not a bot. <span className="learn-more">Learn more</span>.
+                </div>
             </div>
         </form>
     )
