d027b5630cec9a93d1a76fac38ea15be628cd3d0
Naya Willis <whatyoucodingboutwillis@Nayas-MacBook-Pro.local>
2020-09-01 08:06:27 -0400

    testing skeleton loading

--- src/Global/components/Movie.jsx
+++ src/Global/components/Movie.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
+import '../../assets/Movie/movie.css'
+import Skeleton from 'react-loading-skeleton'
 
 const Movie = ({ movie }) => {
     return (
         <li key={movie.id} className="movie-info">
             <div className="poster">
-                <img src={movie.attributes.poster} alt={ movie.attributes.title }/>
+                <img src={movie.attributes.poster} alt={ movie.attributes.title || <Skeleton />}/>
             </div>
-            <div className="content">
-                <p>{movie.attributes.title}</p>
-                <p>{movie.attributes.genre}</p>
-                <p>{movie.attributes.country}</p>
-                <p>{movie.attributes.actors}</p>
-                <p>{movie.attributes.imdb_rating}</p>
-                <p>{movie.attributes.language}</p>
-                <p>{movie.attributes.plot}</p>
-                <p>{movie.attributes.production}</p>
-                <p>{movie.attributes.runtime}</p>
-                <p>{movie.attributes.release_year}</p>
+            <div className="content" >
+                <p>{movie.attributes.title || <Skeleton />}</p>
+                <p>{movie.attributes.genre || <Skeleton />}</p>
+                <p>{movie.attributes.country || <Skeleton />}</p>
+                <p>{movie.attributes.actors || <Skeleton />}</p>
+                <p>{movie.attributes.imdb_rating || <Skeleton />}</p>
+                <p>{movie.attributes.language || <Skeleton />}</p>
+                <p>{movie.attributes.plot || <Skeleton />}</p>
+                <p>{movie.attributes.production || <Skeleton />}</p>
+                <p>{movie.attributes.runtime || <Skeleton />}</p>
+                <p>{movie.attributes.release_year || <Skeleton />}</p>
             </div>
         </li>
     )
