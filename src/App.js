fb80dcb5fe7c46220d98b2a31115de6013536ad3
Naya Willis <whatyoucodingboutwillis@Nayas-MacBook-Pro.local>
2020-09-03 02:29:03 -0400

    created handleLoginRequest method

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import AllMoviesContainer from './Pages/Home/AllMovies/containers/AllMoviesConta
 import EntryPage from './Pages/LoginSIgnupPage/containers/EntryPage';
 import Header from './Global/components/Header';
 import { Switch, Route } from 'react-router-dom'
+import axios from 'axios'
 
 function App() {
+
+  const handleLoginRequest = async (email, password) => {
+    const response = await axios
+      .post(
+        'http://localhost:3001/login',
+        { email, password },
+        { withCredentials: true }
+    )
+    console.log(response)
+  }
   return (
     <div className="App">
         <Switch>
-
           {/* Route to all movies page  */}
           <Route
             exact
@@ -16,14 +26,14 @@ function App() {
             render={
               props => <AllMoviesContainer {...props} />
             }
-          />
+        />
         
           {/* Route to login and signup page  */}
           <Route
             exact
             path="/"
             render={
-              props => <EntryPage {...props}  />
+              props => <EntryPage {...props} handleLoginRequest={handleLoginRequest}  />
             }
           />
         </Switch>
