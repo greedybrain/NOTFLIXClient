766014fb06952115af17e71b88ae750845e46fb4
Naya Willis <whatyoucodingboutwillis@Nayas-MacBook-Pro.local>
2020-09-03 02:31:52 -0400

    created SortMostRecent component

---
+++ src/Pages/Home/AllMovies/components/SortMostRecent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SortMostRecent = ({ sortMostRecent }) => {
+    return (
+        <button onClick={sortMostRecent}>Newest</button>
+    )
+}
+
+export default SortMostRecent
