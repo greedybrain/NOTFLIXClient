0ff26bc48213ae6b3a16b1c6e7f2c389d4c00ebf
Naya Willis <whatyoucodingboutwillis@Nayas-MacBook-Pro.local>
2020-09-03 02:32:09 -0400

    created SortOldestButton component

---
+++ src/Pages/Home/AllMovies/components/SortOldestButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SortOldestButton = ({ sortOldest }) => {
+    return (
+        <button onClick={sortOldest}>Oldest</button>
+    )
+}
+
+export default SortOldestButton
