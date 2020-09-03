d251f297f83b0a18f827ef6437c9daee966643d9
Naya Willis <whatyoucodingboutwillis@Nayas-MacBook-Pro.local>
2020-09-03 02:30:33 -0400

    removed allMovies variable

--- src/Global/components/MovieList.jsx
+++ src/Global/components/MovieList.jsx
@@ -2,11 +2,8 @@ import React from 'react'
 import Movie from './Movie'
 import '../../assets/MovieList/movieList.css'
 
-
 const MovieList = ({ movies }) => {
-    // const allMovies = movies.sort((a, b) => {
-    //     return a.attributes.title.localeCompare(b.attributes.title)
-    // })
+    
     const renderMovies = movies.map(movie => {
         return <Movie key={movie.id} movie={movie} />
     })
