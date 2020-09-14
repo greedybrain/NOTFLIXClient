b439481db3fbcb2c8634b6282467962a98e7e9f8
WhatYouCodingBoutWillis <whatyoucodingboutwillis@Nayas-MBP.localdomain>
2020-09-14 10:59:48 -0400

    removed local state

--- src/App.js
+++ src/App.js
@@ -1,77 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import AllMoviesContainer from './Pages/Home/AllMovies/containers/AllMoviesContainer';
 import EntryPage from './Pages/LoginSIgnupPage/containers/EntryPage';
-import Header from './Global/components/Header';
-import { Switch, Route, Redirect, useHistory } from 'react-router-dom'
-import axios from 'axios'
+import { Switch, Route, Redirect } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux';
+import { checkLoggedInStatus } from './Store/middleware/authUsers';
 
 function App() {
-  const [loggedInStatus, setLoggedInStatus] = useState('NOT_LOGGED_IN')
-  const [user, setUser] = useState({})
-  const [userMovies, setUserMovies] = useState([])
-  const [username, setUsername] = useState('')
-  const [userId, setUserId] = useState('')
-  const history = useHistory()
-
-  const checkLoggedInStatus = async () => {
-    try {
-      const response = await axios.get("http://localhost:3001/logged_in", {
-        withCredentials: true
-      })
-      if (response.data.logged_in && loggedInStatus === "NOT_LOGGED_IN") {
-        setLoggedInStatus('LOGGED_IN')
-        setUser(response.data.user)
-        setUserMovies(response.data.user.movies)
-        setUsername(response.data.user.username)
-        setUserId(response.data.user.id)
-      } else if (!response.data.logged_in && loggedInStatus === "LOGGED_IN") {
-        setLoggedInStatus('NOT_LOGGED_IN')
-        setUser({})
-        setUserMovies([])
-        setUsername('')
-        setUserId('')
-      }
-    } catch (e) {
-      console.log(e)
-    }
-  }
+  const user = useSelector(state => ({
+    status: state.userRed.loggedInStatus
+  }))
+  const dispatch = useDispatch()
 
   useEffect(() => {
-    checkLoggedInStatus()
+    dispatch(checkLoggedInStatus())
   })
-
-  const handleLogin = async (email, password) => {
-    try {
-      const response = await axios.post(
-        "http://localhost:3001/sessions",
-        { email, password },
-        { withCredentials: true }
-      )
-      setLoggedInStatus("LOGGED_IN")
-      setUser(response.data)
-      setUserMovies(response.data.user.data.attributes.movies)
-      setUsername(response.data.user.data.attributes.username)
-      setUserId(response.data.user.data.id)
-      this.props.history.replace('/all_movies')
-    } catch(e) {
-      console.log(e)
-    }
-  }
-
-  const handleLogout = async () => {
-    try {
-      const response = await axios.delete("http://localhost:3001/logout", {
-        withCredentials: true
-      })
-      setLoggedInStatus("NOT_LOGGED_IN")
-      setUser({})
-      setUserMovies([])
-      setUsername('')
-      setUserId('')
-    } catch(e) {
-      console.log(e)
-    }
-  }
 
   return (
     <div className="App">
@@ -81,14 +23,7 @@ function App() {
             exact
             path="/all_movies"
             render={
-              props => <AllMoviesContainer {...props} 
-              loggedInStatus={loggedInStatus} 
-              username={username} 
-              userMovies={userMovies}
-              setUserMovies={setUserMovies}
-              handleLogout={handleLogout}
-              userId={userId}
-              />
+              props => <AllMoviesContainer {...props} />
             }
           />
         
@@ -97,7 +32,7 @@ function App() {
             exact
             path="/"
             render={
-              props => loggedInStatus === "LOGGED_IN" ? <Redirect to="/all_movies" /> : <EntryPage {...props} handleLogin={handleLogin} />
+              props => user.status === "LOGGED_IN" ? <Redirect to="/all_movies" /> : <EntryPage {...props} />
             }
           />
         </Switch>
